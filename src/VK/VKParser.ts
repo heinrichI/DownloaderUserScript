1a880a9597b789f9be2356f213ae3244316a513a
heinrichI <jeronimo73@gmail.com>
2022-08-04 19:22:17 +0300

    VKParser

--- src/VK/VKParser.ts
+++ src/VK/VKParser.ts
@@ -1,3 +1,11 @@
 export function Parse(input: string) 
 {
+    const start: string = "<a class=\"page_doc_icon page_doc_icon1\" href=\"\/";
+
+    let index: number = input.indexOf(start);
+
+    let res = input.substr(start[, length]);
+
+    const numbers: string[] = [];
+    var length = numbers.push(res); 
 }
--- test/vk.test.ts
+++ test/vk.test.ts
@@ -1,10 +1,11 @@
 //import get_wall from './files/get_wall.json'
 //let jsonData = require('./JsonFile.json')
-//import fs from 'fs';
-const fs = require('fs');
-//import path from 'path';
-const path = require('path');
-const vkParser = require('c:/SourceJavaScript/DownloaderUserScript/src/VK/VKParser.ts');
+import fs from 'fs';
+//const fs = require('fs');
+import path from 'path';
+//const path = require('path');
+//const vkParser = require('c:/SourceJavaScript/DownloaderUserScript/src/VK/VKParser.ts');
+import { Parse } from '../src/VK/VKParser';
 
 let getWallPath = path.resolve(__dirname, './files/get_wall.json');
 let jsonData = JSON.parse(fs.readFileSync(getWallPath, 'utf-8'));
@@ -16,8 +17,16 @@ let jsonData = JSON.parse(fs.readFileSync(getWallPath, 'utf-8'));
 describe('testing index file', () => {
      test('Vk extract link', () => 
      {
-          vkParser.Parse(jsonData);
+          Parse(jsonData);
           //const data = await readFile('./files/get_wall.json').then(json => JSON.parse(json)).catch(() => null);
           expect(jsonData).not.toBeNull();
      });
 });
+
+
+// test('Vk extract link', () => 
+// {
+//      Parse(jsonData);
+//      //const data = await readFile('./files/get_wall.json').then(json => JSON.parse(json)).catch(() => null);
+//      expect(jsonData).
+// });
